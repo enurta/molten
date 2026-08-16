068862efb794bc400b4ec8c7287628245a8c44b4
enurta <58881134+enurta@users.noreply.github.com>
2026-08-17 00:33:29 +0900

    Update safe_.sh

--- safe_.sh
+++ safe_.sh
@@ -13,3 +13,17 @@ python -c "from safe.trainer.cli import main; main()" \
     --max_steps 5
 
 python -m safe.trainer.cli --help
+python -m safe.trainer.cli \
+    --config <path to config> \
+    --model-path <path to model> \
+    --tokenizer <path to tokenizer> \
+    --dataset <path to dataset> \
+    --num_labels 9 \
+    --torch_compile True \
+    --optim adamw_torch \
+    --learning_rate 1e-5 \
+    --prop_loss_coeff 1e-3 \
+    --gradient_accumulation_steps 1 \
+    --output_dir "<path to outputdir>" \
+    --max_steps 5 \
+    --do_train True
